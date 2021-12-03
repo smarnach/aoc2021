1181e58cbb99146f4bc3570799fad9294984961b
Sven Marnach <sven@mozilla.com>
2021-12-03 14:04:01 +0100

    Day 03: Make bits_to_int() a bit less generic.

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,17 +1,14 @@
 #![feature(io_read_to_string)]
 
 use anyhow::{Context, Error, Result};
-use std::{
-    borrow::Borrow,
-    io::{read_to_string, stdin},
-};
+use std::io::{read_to_string, stdin};
 
 fn main() -> Result<()> {
     let values = parse_input(&read_to_string(&mut stdin())?)?;
 
     let len = values[0].len();
     let most_common: Vec<_> = (0..len).map(|i| most_common_bit(&values, i)).collect();
-    let gamma = bits_to_int(&most_common);
+    let gamma = bits_to_int(most_common.iter().copied());
     let epsilon = bits_to_int(most_common.iter().map(|b| b ^ 1));
     println!("{}", gamma * epsilon);
 
@@ -28,12 +25,8 @@ fn most_common_bit(values: &[Vec<u8>], index: usize) -> u8 {
     (ones >= (values.len() + 1) / 2) as u8
 }
 
-fn bits_to_int<I>(v: I) -> u32
-where
-    I: IntoIterator,
-    I::Item: Borrow<u8>,
-{
-    v.into_iter().fold(0, |i, b| i << 1 | *b.borrow() as u32)
+fn bits_to_int<I: IntoIterator<Item = u8>>(v: I) -> u32 {
+    v.into_iter().fold(0, |i, b| i << 1 | b as u32)
 }
 
 fn filter(mut values: Vec<Vec<u8>>, invert: u8) -> Result<Vec<u8>> {
