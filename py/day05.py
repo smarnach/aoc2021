08693485bd3a1906bb3a8f1a890cd6ee72f6a4b0
Sven Marnach <sven@mozilla.com>
2021-12-05 20:54:21 +0100

    Day 05: Simplify Python solution at slight runtime cost.

--- py/day05.py
+++ py/day05.py
@@ -14,19 +14,15 @@ def line_points(x0, y0, x1, y1):
         x0 += dx
         y0 += dy
 
-def overlap_count(counts, lines):
-    counts.update(p for line in lines for p in line_points(*line))
+def overlap_count(lines):
+    counts = Counter(p for line in lines for p in line_points(*line))
     return sum(v > 1 for v in counts.values())
 
 def main():
     lines = [tuple(map(int, re.findall("\d+", line))) for line in sys.stdin]
-    straight = []
-    diagonal = []
-    for x0, y0, x1, y1 in lines:
-        (straight if x0 == x1 or y0 == y1 else diagonal).append((x0, y0, x1, y1))
-    counts = Counter()
-    print(overlap_count(counts, straight))
-    print(overlap_count(counts, diagonal))
+    straight = [(x0, y0, x1, y1) for x0, y0, x1, y1 in lines if x0 == x1 or y0 == y1]
+    print(overlap_count(straight))
+    print(overlap_count(lines))
 
 if __name__ == "__main__":
     main()
