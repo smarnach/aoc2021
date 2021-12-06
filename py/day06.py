8ef4e0d09140eea8d6148d735adb04de79a19d62
Sven Marnach <sven@mozilla.com>
2021-12-06 13:22:24 +0100

    Day 05: Simplify parsing in Python solution.

--- py/day06.py
+++ py/day06.py
@@ -4,13 +4,12 @@ import numpy as np
 import sys
 
 def main():
-    school = np.zeros((9, 1), dtype=int)
-    for fish in sys.stdin.read().split(","):
-        school[int(fish), 0] += 1
+    school = np.bincount(sys.stdin.read().split(","))
+    school.resize((9, 1))
     a = np.matrix(np.eye(9, k=1, dtype=int))
-    a[6, 0] = a[8, 0] = 1
-    print((a ** 80 * school).sum())
-    print((a ** 256 * school).sum())
+    a[[6, 8], 0] = 1
+    print((a ** 80 @ school).sum())
+    print((a ** 256 @ school).sum())
 
 if __name__ == "__main__":
     main()
