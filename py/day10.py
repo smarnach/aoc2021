d22322b266323013f3bea81e29b0555c942de8e7
Sven Marnach <sven@mozilla.com>
2021-12-10 16:17:43 +0100

    Day 10: Use hacky regex approach in Python solution.

--- py/day10.py
+++ py/day10.py
@@ -2,26 +2,23 @@
 
 from functools import reduce
 from statistics import median
+import re
 import sys
 
-CLOSING = {"(": ")", "[": "]", "{": "}", "<": ">"}
 SYNTAX = {")": 3, "]": 57, "}": 1197, ">": 25137}
-COMPLETE = {")": 1, "]": 2, "}": 3, ">": 4}
+COMPLETE = {"(": 1, "[": 2, "{": 3, "<": 4}
 
 def main():
     syntax = 0
     complete = []
     for line in sys.stdin:
-        stack = []
-        for c in line.strip():
-            if d := CLOSING.get(c):
-                stack.append(d)
-            else:
-                if c != stack.pop():
-                    syntax += SYNTAX[c]
-                    break
+        n = 1
+        while n:
+            line, n = re.subn(r"\(\)|\[\]|\{\}|<>", "", line.strip())
+        if corrupt := re.search(r"[])}>]", line):
+            syntax += SYNTAX[corrupt[0]]
         else:
-            score = reduce(lambda x, c: 5 * x + COMPLETE[c], stack[::-1], 0)
+            score = reduce(lambda x, c: 5 * x + COMPLETE[c], line[::-1], 0)
             complete.append(score)
     print(syntax)
     print(median(complete))
